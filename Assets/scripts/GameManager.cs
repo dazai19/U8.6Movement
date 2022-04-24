8560549ff7e4179bb70c54191034feac5b198160
Artur Israilov <a.israilov@jsa-group.ru>
2022-04-24 17:05:22 +0300

    add movements based on the principle of a relay race

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,21 +7,30 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] pointsOne;
     [SerializeField] private GameObject[] pointsTwo;
-    [SerializeField] private GameObject runnerOne, runnerTwo;
+    [SerializeField] private GameObject runnerOne, runnerTwo, runnerThree, runnerFour;
     
     Vector3[] massVctPointOne = new Vector3[13];
     Vector3[] massVctPointTwo = new Vector3[13];
     private Vector3 targetVctOne = new Vector3();
     private Vector3 targetVctTwo = new Vector3();
 
+    private GameObject activRunnerOne, activeRunnerTwo, passiveRunnerOne, passiveRunnerTwo, swapObject;
+
     private int pointN1 = 0, pointN2 = 0, 
                 speed1, speed2;
+
+    private float distOneThree, distTwoFour;
+    private bool check1 = true, check2 = true;
 
     Random rand = new Random();
 
 
     void Start()
     {
+        activRunnerOne = runnerOne;
+        activeRunnerTwo = runnerTwo;
+        passiveRunnerOne = runnerThree;
+        passiveRunnerTwo = runnerFour;
         CreatePointVector(massVctPointOne, pointsOne);
         CreatePointVector(massVctPointTwo, pointsTwo);
 
@@ -33,9 +42,37 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        Movement(runnerOne, massVctPointOne, targetVctOne, speed1, true);
-        Movement(runnerTwo, massVctPointTwo, targetVctTwo, speed2, false);
+        passiveRunnerOne.transform.LookAt(activRunnerOne.transform);
+        passiveRunnerTwo.transform.LookAt(activeRunnerTwo.transform);
 
+        distOneThree = Vector3.Distance(activRunnerOne.transform.position, passiveRunnerOne.transform.position);
+        distTwoFour = Vector3.Distance(activeRunnerTwo.transform.position, passiveRunnerTwo.transform.position);
+
+        if (distOneThree <= 0.7f && check1)
+        {
+            swapObject = activRunnerOne;
+            activRunnerOne = passiveRunnerOne;
+            passiveRunnerOne = swapObject;
+            check1 = false;
+        }
+        else if (distOneThree > 1)
+            check1 = true;
+
+        if (distTwoFour <= 0.7f && check2)
+        {
+            swapObject = activeRunnerTwo;
+            activeRunnerTwo = passiveRunnerTwo;
+            passiveRunnerTwo = swapObject;
+            check2 = false;
+        }
+        else if (distTwoFour > 1)
+            check2 = true;
+
+        Movement(activRunnerOne, massVctPointOne, targetVctOne, speed1, true);
+        Movement(activeRunnerTwo, massVctPointTwo, targetVctTwo, speed2, false);
+
+        passiveRunnerOne.transform.position = Vector3.MoveTowards(passiveRunnerOne.transform.position, massVctPointOne[6], Time.deltaTime);
+        passiveRunnerTwo.transform.position = Vector3.MoveTowards(passiveRunnerTwo.transform.position, massVctPointTwo[7], Time.deltaTime);
     }
 
     private void CreatePointVector(Vector3[] inputvector, GameObject[] inputobject)
@@ -76,14 +113,6 @@ public class GameManager : MonoBehaviour
 
                 target = targetVctTwo;
             }
-
-            //current++;
-            //if (current < massVct.Length)
-            //{
-            //    target = massVct[current];
-            //}
-            //else if (current == massVct.Length)
-            //    current = 0;
         }
 
         runner.transform.LookAt(target);
